8c9a5c110b29ad636b48f1378f85456f534dfdff
Denis Sidorov <cto@ambersoft.llc>
2021-08-24 14:38:04 +0300

    add email endpoint

--- routes/infoRouter.js
+++ routes/infoRouter.js
@@ -1,9 +1,21 @@
 const express = require('express')
 const router = express.Router()
+const notificationModel = require('../models/notification')
 
 router.get('/', [
 ], (req, res) => {
   res.json(require('../counters.json'))
+})
+
+router.post('/email', [
+], async (req, res) => {
+
+  const user = req.body.user
+  const email = req.body.email
+
+  const model = await notificationModel.create(user,email)
+
+  res.json({model})
 })
 
 
