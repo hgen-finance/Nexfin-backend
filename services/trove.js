eebf2516150fafa50abab1ad3723113c4ac00c94
Denis Sidorov <cto@ambersoft.llc>
2021-08-24 11:54:33 +0300

    change state to string

--- services/trove.js
+++ services/trove.js
@@ -20,11 +20,11 @@ const decodeTrove = ({encodedTroveState, trove}) => {
     isInitialized: !!decodedTroveState.isInitialized,
     isReceived: !!decodedTroveState.isReceived,
     isLiquidated: !!decodedTroveState.isLiquidated,
-    borrowAmount: new BN(decodedTroveState.borrowAmount, 10, 'le').toNumber(),
-    lamports: new BN(decodedTroveState.lamports, 10, 'le').toNumber(),
-    teamFee: new BN(decodedTroveState.teamFee, 10, 'le').toNumber(),
-    depositorFee: new BN(decodedTroveState.depositorFee, 10, 'le').toNumber(),
-    amountToClose: new BN(decodedTroveState.amountToClose, 10, 'le').toNumber(),
+    borrowAmount: new BN(decodedTroveState.borrowAmount, 10, 'le').toString(),
+    lamports: new BN(decodedTroveState.lamports, 10, 'le').toString(),
+    teamFee: new BN(decodedTroveState.teamFee, 10, 'le').toString(),
+    depositorFee: new BN(decodedTroveState.depositorFee, 10, 'le').toString(),
+    amountToClose: new BN(decodedTroveState.amountToClose, 10, 'le').toString(),
     owner: new PublicKey(decodedTroveState.owner).toBase58(),
   }
 }
