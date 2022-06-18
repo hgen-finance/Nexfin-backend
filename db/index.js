8d197100f1852aeb2758d8deecfb731b56e6ac00
AI-Pree <sexypistolcs@gmail.com>
2022-06-18 13:59:06 +1000

    server test update

--- db/index.js
+++ db/index.js
@@ -1,13 +1,13 @@
-// require('dotenv').config()
+require('dotenv').config()
 const mongoose = require("mongoose");
 
 //TODO: Add the URI of mongodb to env
 mongoose
-  .connect("mongodb://mongo-nexfin:27017/data", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => console.log("MongoDB Connected"))
-  .catch((err) => console.log(err));
+    .connect(`mongodb+srv://root:${process.env.DB_PASSWORD}@transaction-log-devnet.o4s3r.mongodb.net/?retryWrites=true&w=majority`, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(() => console.log("MongoDB Connected"))
+    .catch((err) => console.log(err));
 
 module.exports = mongoose;
