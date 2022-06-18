bbcd7015f08ae6a594e3927a99b276bc96c814b5
pree-side <ppreeyash@gmail.com>
2022-06-18 15:28:12 +1000

    change api

--- app.js
+++ app.js
@@ -18,37 +18,37 @@ app.use(cors());
 
 app.use(express.json({ limit: "50mb" }));
 app.use(
-  express.urlencoded({
-    extended: true,
-    limit: "50mb",
-  })
+    express.urlencoded({
+        extended: true,
+        limit: "50mb",
+    })
 );
 
 app.use(cookieParser());
 
-app.use("/trove", troveRouter);
-app.use("/deposit", depositRouter);
-app.use("/reward", rewardRouter);
-app.use("/info", infoRouter);
+app.use("/api/trove", troveRouter);
+app.use("/api/deposit", depositRouter);
+app.use("/api/reward", rewardRouter);
+app.use("/api/info", infoRouter);
 //app.use("/notification", notificationRouter);
-app.get("/", (req, res) => {
-  res.send({});
+app.get("/api", (req, res) => {
+    res.send({});
 });
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
-  next(createError(404));
+    next(createError(404));
 });
 
 // error handler
 app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.json({ err });
-  res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
+    // set locals, only providing error in development
+    res.json({ err });
+    res.locals.message = err.message;
+    res.locals.error = req.app.get("env") === "development" ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
+    // render the error page
+    res.status(err.status || 500);
 });
 
 module.exports = app;
