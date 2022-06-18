88a88f472a4a1eb10efd17f16b70397ba0f0b8fa
AI-Pree <sexypistolcs@gmail.com>
2022-06-18 14:46:36 +1000

    remove test code

--- db/index.js
+++ db/index.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 const mongoose = require("mongoose");
 
-//TODO: Add the URI of mongodb to env for testing
+//TODO: Add the URI of mongodb to env 
 mongoose
     .connect(`mongodb+srv://root:${process.env.DB_PASSWORD}@transaction-log-devnet.o4s3r.mongodb.net/?retryWrites=true&w=majority`, {
         useNewUrlParser: true,
